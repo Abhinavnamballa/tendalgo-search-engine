7b0c80913eaeeec547a8368708d89cd5b0f9f9a6
Abhi Namballa <namballaabhi@gmail.com>
2020-08-22 21:09:57 -0400

    Updates

---
+++ precache-manifest.5c43b57283722841570a1ade40bb25b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c3f07740a2291c0d3c1cc21fe577c4",
+    "url": "/tendalgo-search-engine/index.html"
+  },
+  {
+    "revision": "e21467e234114cbb2a76",
+    "url": "/tendalgo-search-engine/static/css/main.45cdea0e.chunk.css"
+  },
+  {
+    "revision": "53707d9b5d0780eb4b52",
+    "url": "/tendalgo-search-engine/static/js/2.7ccde67e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tendalgo-search-engine/static/js/2.7ccde67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21467e234114cbb2a76",
+    "url": "/tendalgo-search-engine/static/js/main.b738c8db.chunk.js"
+  },
+  {
+    "revision": "a9c995b8b3385cd062be",
+    "url": "/tendalgo-search-engine/static/js/runtime-main.86a75e04.js"
+  }
+]);
